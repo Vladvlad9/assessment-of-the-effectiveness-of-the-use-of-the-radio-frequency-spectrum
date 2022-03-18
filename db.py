a5574d175af6f214a80cffbe0c731bf3b488ef75
Vlad Paulechka <vlad9.ru@gmail.com>
2022-03-18 17:57:13 +0300

    create new functions add_basic_formula and get_basic_formula

--- db.py
+++ db.py
@@ -24,7 +24,7 @@ class DBApi(object):
         result = "SELECT * FROM technical_efficiency"
         return self.__cur.execute(result).fetchall()
 
-    def select_basic_formula(self, E= 0, C1: str = '', C2 = 0, C3 = 0):
+    def insert_basic_formula(self, E= 0, C1: str = '', C2 = 0, C3 = 0):
         try:
             self.__cur.execute('''
                         INSERT INTO basic_formula(
@@ -40,6 +40,15 @@ class DBApi(object):
             return False
         else:
             return True
+
+    def add_basic_formula(self, row: str, value: str) -> None:
+        res = f'UPDATE basic_formula SET {row} = {value} WHERE id = 1'
+        self.__cur.execute(res)
+        self.__conn.commit()
+
+    def get_basic_formula(self):
+        result = "SELECT * FROM basic_formula"
+        return self.__cur.execute(result).fetchall()
 
 
     def create_all_database(self) -> None:
